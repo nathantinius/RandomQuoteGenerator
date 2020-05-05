5a7e28af1ee00e0084666d42dac9bda4f399466a
Nathan Tinius <nathantinius@gmail.com>
2020-05-04 19:54:52 -0500

    Initalize the Repo.

---
+++ RandomQuoteGenerator.js
@@ -0,0 +1,46 @@
+const quotes = [
+    {
+        source: 'Ezra Tinius',
+        quote: "Spank it mom!",
+        year: 2018,
+        age: 4,
+    },
+    {
+        source: "SpongeBob Squarepants",
+        quote: "I'm ready... promotion!",
+    },
+    {
+        source: "Oscar Wilde",
+        quote: "Be yourself; everyone else is already taken.",
+        citation: "https://quotesnsmiles.com/quotes/77-of-the-worlds-most-popular-quotes/"
+    },
+    {
+        source: "Dr Seuss",
+        quote: "You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose. You're on your own. And you know what you know. And YOU are the one who'll decide where to go...",
+        citation: "Oh The Places You'll Go!",
+    },
+    {
+        source: "Dory",
+        quote: "P. Sherman, 42 Wallaby Way, Sydney.",
+        citation: "Finding Nemo",
+    },
+    {
+        source: "Jake Paralta",
+        quote: "Oh... cool cool cool cool cool cool cool cool cool cool cool cool.",
+        citation: "Brooklyn 99",
+    },
+    {
+        source: "Peter Quill",
+        quote: "Remember, I'm only half human. So that 50% of me that's stupid... that's 100% you.",
+        citation: "Avengers: Infinity War",
+    },
+    {
+        source: "Obi-Wan Kanobe",
+        quote: "You can't win Darth. If you strike me down and I will become more powerful than you can possibly imagine.",
+        citation: "Star Wars: A New Hope"
+    }
+]
+
+function GetRandomQuote() {
+
+}
